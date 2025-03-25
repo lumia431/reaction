14a904ce10c832e0e4e2d5a468569bbf0159e2b7
lumia431 <844727475@qq.com>
2025-03-25 16:25:22 +0800

    final version

--- include/reaction/log.h
+++ include/reaction/log.h
@@ -4,40 +4,34 @@
 
 #include <iostream>
 #include <string>
-#include <format> // C++20的std::format
+#include <format>
 
-namespace reaction
-{
-    class Log
-    {
-    public:
-        template <typename... Args>
-        static void info(const std::string &format_str, Args &&...args)
-        {
-            log("INFO", format_str, std::forward<Args>(args)...);
-        }
+namespace reaction {
+class Log {
+public:
+    template <typename... Args>
+    static void info(const std::string &format_str, Args &&...args) {
+        log("INFO", format_str, std::forward<Args>(args)...);
+    }
 
-        template <typename... Args>
-        static void warn(const std::string &format_str, Args &&...args)
-        {
-            log("WARN", format_str, std::forward<Args>(args)...);
-        }
+    template <typename... Args>
+    static void warn(const std::string &format_str, Args &&...args) {
+        log("WARN", format_str, std::forward<Args>(args)...);
+    }
 
-        template <typename... Args>
-        static void error(const std::string &format_str, Args &&...args)
-        {
-            log("ERROR", format_str, std::forward<Args>(args)...);
-        }
+    template <typename... Args>
+    static void error(const std::string &format_str, Args &&...args) {
+        log("ERROR", format_str, std::forward<Args>(args)...);
+    }
 
-    private:
-        template <typename... Args>
-        static void log(const std::string &level, const std::string &format_str, Args &&...args)
-        {
-            auto fmt_args{std::make_format_args(args...)};
-            std::string message = std::vformat(format_str, fmt_args);
-            std::cout << "[" << level << "] " << message << std::endl;
-        }
-    };
+private:
+    template <typename... Args>
+    static void log(const std::string &level, const std::string &format_str, Args &&...args) {
+        auto fmt_args{std::make_format_args(args...)};
+        std::string message = std::vformat(format_str, fmt_args);
+        std::cout << "[" << level << "] " << message << std::endl;
+    }
+};
 
 } // namespace reaction
 
